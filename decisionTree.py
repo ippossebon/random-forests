8e3ca9a76a5d2322ed19e2fd6d43efa34f021686
Isadora <isadorapossebon@gmail.com>
2018-09-16 16:31:16 -0300

    Save work with different algorithm for decision tree

--- decisionTree.py
+++ decisionTree.py
@@ -9,11 +9,34 @@ class DecisionTree:
         self.tree = None
         self.is_tree_initialized = False
 
-    def initializeTree(self, value):
-        self.tree = Node(value)
+    def initializeTree(self, root):
+        self.tree = Node(root)
         self.is_tree_initialized = True
 
-    def createTree():
+    def newDictWithoutKey(self, d, key):
+        r = dict(d)
+        del r[key]
+        return r
+
+    def splitTree(self, attribute, instances):
+        for instance in instances:
+            if self.tree.hasChild(instance[attribute]):
+                # Adiciona instancia com o valor deste atributo
+                for index in range(len(self.tree.children)):
+                    if attribute in instance:
+                        if self.tree.children[index].value == instance[attribute]:
+                            new_instance = self.newDictWithoutKey(instance, attribute)
+                            self.tree.children[index].children.append(new_instance)
+            else:
+                # Cria novo nodo com o valor do atributo dessa instância
+                new_node = Node(instance[attribute])
+                new_instance = self.newDictWithoutKey(instance, attribute)
+                new_node.children.append(new_instance)
+                self.tree.children.append(new_node)
+
+        return instances
+
+    def createTree(self):
         instances_copy = list(self.instances)
         # Construção top-down - recursivo
 
@@ -22,21 +45,13 @@ class DecisionTree:
         # 2. Não há mais atributos para escolher ou uma partição resultante é vazia: cria-se uma folha com a classe majoritária dentre as instâncias
 
 
-        # Escolehr um atributo para adicionar à árvore (iniciando pela raiz)
-        attribute_index = 0
+        # Escolher um atributo para adicionar à árvore (iniciando pela raiz)
         for attribute in self.attributes:
             if not self.is_tree_initialized:
                 self.initializeTree(attribute)
 
-            # Estender a árvore, adicionando um ramo para cada valor do atributo selecionado
-            for instance in self.instances_copy:
-                if instance[attribute_index] in self.tree.children:
-                    # Este valor de atributo já tem uma árvore associada
-                    pass
-                else:
-                    # Valor de atributo não tem árvore associada. Cria
-                    self.tree.data = instance[attribute_index]
-                    self.tree.addChild(instance)
+            instances_copy = self.splitTree(attribute, instances_copy)
+        self.tree.printTree()
 
         # Dividir os exemplos em partições (uma para cada ramo adicionado), conforme valores do atributo testado
         # Para cada partição de exemplos resultante, repetir este procedimento recursivamente
