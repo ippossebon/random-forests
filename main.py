6982147fb23637ed510f73e2bad4fab5c5edb14c
Luis Antonio Costa <luis.leite89@gmail.com>
2018-09-25 01:08:26 -0300

    Added prediction function for decision tree. Needs review in identation

--- main.py
+++ main.py
@@ -10,13 +10,20 @@ attributes_type = 'cat' se os atributos são categóricos
 """
 
 def main():
-    file_name = './data/wine.csv'
+    file_name = './data/joga.csv'
     attributes, instances = getDataFromFile(file_name)
 
-    tree = Tree('cat', attributes, 'class', instances)
+    tree = Tree('cat', attributes, 'Joga', instances)
     tree.createDecisionTree()
     tree.printDecisionTree()
-
+    
+    new_file_name = './data/new_data.csv'    
+    attributes, new_instances = getDataFromFile(new_file_name)
+    
+    # Preve a classe de cada nova instancia informada
+    for instance in range(len(new_instances)):
+		predict(attributes, instance, tree)
+	
 # Retorna a lista de atributos e um dicionário de instâncias do problema
 def getDataFromFile(file_name):
     instances = []
@@ -40,6 +47,22 @@ def getDataFromFile(file_name):
                 instances.append(instance)
 
     return attributes, instances
+  
+ # Classifica uma nova instancia de acordo com a arvore de decisao   
+ def predict(attributes, instance, tree, default_class = None):
+	 if not tree:
+		 return default_class
+      
+     attribute_index = list(tree.keys())[0]
+     attribute_values = list(tree.values())[0]
+     instance_attribute_value = instance[attribute_index]
+      
+     if instance_attribute_value not in attribute_values:
+		 return default_class
+     
+     return self._predict(attribute_values[instance_attribute_value],
+                          instance, default_class)
+    
 
 if __name__ == '__main__':
     main()
---
+++ predict.py
@@ -0,0 +1,16 @@
+import csv
+
+from node import Node
+from tree import Tree
+
+def main():
+	
+	while (
+	print ('Informe as instancias: (Digite . para terminar)')
+
+
+def predict(filename, tree):
+	
+	attributes, instances = getDataFromFile(file_name)
+	
+	tree = 
